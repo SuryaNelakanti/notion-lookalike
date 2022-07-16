1e907004744e7deaa8f783bf3c26dab58af56a2f
surya_nelakanti <nelakantisuryateja@gmail.com>
2022-07-16 10:17:52 +0530

    intial commit, base set up completed

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './assets/scss/main.scss';
+
+export const App = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
---
+++ src/root.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const Root = () => {
+  return <App />;
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
