77846bee42d54814a76c849002ca4675d82da134
surya_nelakanti <nelakantisuryateja@gmail.com>
2022-07-16 13:39:01 +0530

    drag and drop functionality working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import './assets/scss/main.scss';
-import { Table } from './components/table/table';
+import React from 'react'
+import './assets/scss/main.scss'
+import { Table } from './components/table/table'
+
+const groupNotes = [
+  { name: 'Group 1', value: ['Test 1', 'Test 2'] },
+  { name: 'Group 2', value: ['Test 3', 'Test 4'] },
+  { name: 'Group 3', value: ['Test 5', 'Test 6'] },
+]
 export const App = () => {
   return (
     <div className="App">
-      <Table></Table>
+      <Table groupNotes={groupNotes}></Table>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,60 +1,67 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
+
+type groupNoteType = {
+  name: string
+  value: Array<string>
+}
 
 type TableProps = {
-  groups?: Array<string>
-  items?: Array<string>
+  groupNotes: Array<groupNoteType>
 }
-export const Table: React.FC<TableProps> = ({ groups: columns, items }) => {
-  console.log(columns)
-  console.log(items)
+export const Table: React.FC<TableProps> = ({ groupNotes }) => {
+  const [groupNotesState, setGroupNotesState] =
+    useState<Array<groupNoteType>>(groupNotes)
   const dragStartIndex = useRef({ groupIndex: 0, noteIndex: 0 })
   const dragEndIndex = useRef({ groupIndex: 0, noteIndex: 0 })
 
-  const groupNotes = [
-    { name: 'Group 1', value: ['Test 1', 'Test 2'] },
-    { name: 'Group 2', value: ['Test 3', 'Test 4'] },
-    { name: 'Group 3', value: ['Test 1', 'Test 2'] },
-  ]
-
-  const dragStart = (
-    dragEvent: React.DragEvent<HTMLDivElement>,
-    groupIndex: number,
-    noteIndex: number
-  ) => {
-    console.log('DragStart')
-    console.log(dragEvent.target)
-    console.log(groupIndex)
-    console.log(noteIndex)
-
+  const draggingStartHandler = (groupIndex: number, noteIndex: number) => {
     dragStartIndex.current.groupIndex = groupIndex
     dragStartIndex.current.noteIndex = noteIndex
   }
 
-  const dragEnd = (
-    dragEndEvent: React.DragEvent<HTMLDivElement>,
-    groupIndex: number,
-    noteIndex: number
-  ) => {
-    console.log('DragEnd')
-    console.log(dragEndEvent.target)
-    console.log(groupIndex)
-    console.log(noteIndex)
-
+  const draggedOverHandler = (groupIndex: number, noteIndex: number) => {
     dragEndIndex.current.groupIndex = groupIndex
     dragEndIndex.current.noteIndex = noteIndex
   }
 
+  const dragEnd = () => {
+    const updatedGroupNotes = [...groupNotesState]
+    // Index of the note selected and the group it is selected in.
+    const initialGroupIndex = dragStartIndex.current.groupIndex
+    const initialNoteIndex = dragStartIndex.current.noteIndex
+
+    // Index of where the note got dragged to, and the group.
+    const finalGroupIndex = dragEndIndex.current.groupIndex
+    const finalNoteIndex = dragEndIndex.current.noteIndex
+
+    // Note dragged value.
+    const initialNote =
+      updatedGroupNotes[initialGroupIndex].value[initialNoteIndex]
+
+    // Removing inital location of the note.
+    updatedGroupNotes[initialGroupIndex].value.splice(initialNoteIndex, 1)
+
+    // Setting final location of the note.
+    updatedGroupNotes[finalGroupIndex].value.splice(
+      finalNoteIndex,
+      0,
+      initialNote
+    )
+    setGroupNotesState(updatedGroupNotes)
+  }
+
   return (
     <div className="table">
-      {groupNotes.map((group, groupIndex) => (
+      {groupNotesState.map((group, groupIndex) => (
         <div className="group-header header-txt-1 bold-txt" key={groupIndex}>
           {group.name}
           {group.value.map((note, noteIndex) => (
             <div
               className="draggable-items body-txt-2"
               draggable
-              onDragStart={(e) => dragStart(e, groupIndex, noteIndex)}
-              onDragEnter={(e) => dragEnd(e, groupIndex, noteIndex)}
+              onDragStart={() => draggingStartHandler(groupIndex, noteIndex)}
+              onDragEnter={() => draggedOverHandler(groupIndex, noteIndex)}
+              onDragEnd={dragEnd}
               key={noteIndex}
             >
               {note}
